840871c09c11a760706113897ca72811a368f78e
Vincent Corneau <thecreepermdrl@gmail.com>
2024-08-21 17:46:21 -0400

    Update,  added functionnal map drawing

--- 2DGame/Program.cs
+++ 2DGame/Program.cs
@@ -7,7 +7,7 @@ namespace _2DGame
         static void Main(string[] args)
         {
             string[] mapBlockList = new string[16];
-            LoadMapInRam(out mapBlockList, "E:\\2DGame\\2DGame\\data\\map.txt");
+            LoadMapInRam(out mapBlockList, "C:\\Users\\TheyCreeper\\Documents\\Dev\\C#\\2DGame\\2DGame\\data\\map.txt");
             DrawMap(mapBlockList);
 
         }
@@ -15,9 +15,19 @@ namespace _2DGame
         private static void DrawMap(string[] mapBlockList)
         {
             int yCounter = 0;
+            int cCounter = 0;
             while(yCounter < mapBlockList.Length)
             {
-                mapBlockList[yCounter].ToCharArray
+                char[] charList = mapBlockList[yCounter].ToCharArray();
+                while (cCounter < mapBlockList[yCounter].ToCharArray().Length)
+                {
+                    int t = mapBlockList[yCounter].ToCharArray().Length;
+                    DrawBlockWithColor(charList[cCounter]);
+                    cCounter++;
+                }
+                cCounter = 0;
+                Console.WriteLine("");
+                yCounter++;
             }
         }
 
@@ -37,15 +47,13 @@ namespace _2DGame
             }
         }
 
-        private static void DrawItemWithColor( char c)
+        private static void DrawBlockWithColor(char c)
         {
-            
-
             //Retransforme l'input de certaines piece en l'original écris par l'utilisateur. Rend le tout plus consistant avec ce qui est écris dans la console et ce qui est 
             //dessiner dans le board
-            if (c == '-') bgColor = ConsoleColor.Green;
-            if (c == 'T') bgColor = ConsoleColor.DarkGreen;
-            if (c == 'I') bgColor = ConsoleColor.DarkGray;
+            if (c == '-') Console.BackgroundColor = ConsoleColor.Green;
+            if (c == 'T') Console.BackgroundColor = ConsoleColor.DarkGreen;
+            if (c == 'I') Console.BackgroundColor = ConsoleColor.DarkGray;
 
             Console.Write("  ");
             Console.ResetColor();
